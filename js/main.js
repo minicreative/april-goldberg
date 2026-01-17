b7f58d9997162c16f4a8a407819d96b9b701f769
Tomas Roy <hello@minicreative.net>
2026-01-16 19:16:03 -0600

    homepage font size screen-based, started imageScroll, FontAwesome included

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(() => {
-    
+    setupScrollImages()
 })
+
+function setupScrollImages() {
+    let isScrolling = false;
+    $("#scrollImages").on('mouseover', () => {
+    })
+    $("#scrollImages").on('mouseout', () => {
+    })
+}
